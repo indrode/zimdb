1a17530ad472ed2724f6dfb232fea0db33aeef11
Indro De <indro.de@gmail.com>
2011-12-02 20:39:02 +0100

    add finding movie by id example

--- example/zimdb-example.rb
+++ example/zimdb-example.rb
@@ -1,13 +1,22 @@
 require 'zimdb'
 require 'rspec'
 
-def get_movie_year
+def get_movie_year_by_title
   movie = Zimdb::Movie.new(:title => "Hangover")
   movie.year
 end
 
+def get_movie_year_by_id
+  movie = Zimdb::Movie.new(:id => "tt1119646")
+  movie.year
+end
+
 describe "simple zimdb test" do
-  it "should get movie information and return title" do
-    get_movie_year.should eq(2009)
+  it "should get movie information by title and return year" do
+    get_movie_year_by_title.should eq(2009)
+  end
+  
+  it "should get movie information by id and return year" do
+    get_movie_year_by_id.should eq(2009)
   end
 end
